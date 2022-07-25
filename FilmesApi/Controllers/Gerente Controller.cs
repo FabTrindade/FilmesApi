d3c48d8190b425b9d870dd476af99789eadcb7a9
FabTrindade <fabrtrindade@gmail.com>
2022-07-24 22:00:43 -0300

    Adding Gerente model

---
+++ FilmesApi/Controllers/Gerente Controller.cs
@@ -0,0 +1,47 @@
+﻿using AutoMapper;
+using FilmesApi.Data;
+using FilmesApi.Data.Dtos.Gerente;
+using FilmesApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmesApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class Gerente_Controller : ControllerBase
+    {
+        private AppDbContext _context;
+        private IMapper _mapper;
+
+        public Gerente_Controller(AppDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public  IActionResult AdicionaGerente (CreateGerenteDto dto)
+        {
+            Gerente gerente = _mapper.Map<Gerente>(dto);
+            _context.Gerentes.Add(gerente);
+            _context.SaveChanges();
+            return CreatedAtAction(nameof(RecuperaGerentePorId), new { Id = gerente.Id }, gerente);
+
+        }
+
+        public  IActionResult RecuperaGerentePorId(int id)
+        {
+            Gerente gerente = _context.Gerentes.FirstOrDefault(gerente => gerente.Id == id);
+            if (gerente  != null)
+            {
+                ReadGerenteDto gerenteDto = _mapper.Map<ReadGerenteDto>(gerente);
+
+                return Ok(gerenteDto );
+            }
+            return NotFound();
+        }
+    }
+}
