c13cc030d28da81866248529eac4fe5709c4b956
FabTrindade <fabrtrindade@gmail.com>
2022-08-20 21:37:05 -0300

    Creating UsuariosApi project

---
+++ UsuariosApi/Controllers/CadastroController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UsuariosApi.Data.Dtos;
+
+namespace UsuariosApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]    
+    public class CadastroController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult CadastraUsuario(CreateUsuarioDto createDto)
+        {
+            return Ok();
+        }
+    }
+}
