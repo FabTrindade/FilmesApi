6b5b2f25dec90452c45f8505a86c4cacba7a2bc4
FabTrindade <fabrtrindade@gmail.com>
2022-08-20 23:13:18 -0300

    Configuring Identity

---
+++ UsuariosApi/Data/UserDbContext.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UsuariosApi.Data
+{
+    public class UserDbContext : IdentityDbContext<IdentityUser<int>, IdentityRole<int>, int>
+    {
+        public UserDbContext(DbContextOptions<UserDbContext> options) : base(options)
+        {
+        }
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<IdentityUser>(entity =>
+            {
+                entity.Property(m => m.Id).HasMaxLength(110);
+                entity.Property(m => m.Email).HasMaxLength(127);
+                entity.Property(m => m.NormalizedEmail).HasMaxLength(127);
+                entity.Property(m => m.NormalizedUserName).HasMaxLength(127);
+                entity.Property(m => m.UserName).HasMaxLength(127);
+            });
+            modelBuilder.Entity<IdentityRole>(entity =>
+            {
+                entity.Property(m => m.Id).HasMaxLength(110);
+                entity.Property(m => m.Name).HasMaxLength(127);
+                entity.Property(m => m.NormalizedName).HasMaxLength(127);
+            });
+            modelBuilder.Entity<IdentityUserLogin<int>>(entity =>
+            {
+                entity.Property(m => m.LoginProvider).HasMaxLength(127);
+                entity.Property(m => m.ProviderKey).HasMaxLength(127);
+                entity.HasKey("LoginProvider", "ProviderKey");
+            });
+            modelBuilder.Entity<IdentityUserRole<string>>(entity =>
+            {
+                entity.Property(m => m.UserId).HasMaxLength(127);
+                entity.Property(m => m.RoleId).HasMaxLength(127);
+                entity.HasKey("UserId", "RoleId");
+            });
+            modelBuilder.Entity<IdentityUserToken<int>>(entity =>
+            {
+                entity.Property(m => m.UserId).HasMaxLength(110);
+                entity.Property(m => m.LoginProvider).HasMaxLength(110);
+                entity.Property(m => m.Name).HasMaxLength(110);
+                entity.HasKey("UserId", "LoginProvider", "Name"); ;
+
+            });
+        }
+    }
+}
