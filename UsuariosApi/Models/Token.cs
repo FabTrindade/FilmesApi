7c244e69e7b2079a970144e051223e9c68a5b4ab
FabTrindade <fabrtrindade@gmail.com>
2022-08-21 20:04:39 -0300

    Creating TokenService

---
+++ UsuariosApi/Models/Token.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UsuariosApi.Models
+{
+    public class Token
+    {
+        public Token(string value)
+        {
+            Valor = value;
+        }
+        public string Valor { get;}
+    }
+}
---
+++ UsuariosApi/Services/TokenService.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using UsuariosApi.Models;
+
+namespace UsuariosApi.Services
+{
+    public class TokenService
+    {
+        public Token CreateToken(IdentityUser<int> usuario)
+        {
+            Claim[] direitosUsuario = new Claim[]
+            {
+                new Claim("username", usuario.UserName),
+                new Claim("id", usuario.Id.ToString()),
+            };
+
+            var chave = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes("0asdf00asdf00asdf0a0sd0f0as0df0as0df00876afsd")
+                );
+            var credentials = new SigningCredentials(chave, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                claims: direitosUsuario,
+                signingCredentials: credentials,
+                expires: DateTime.UtcNow.AddHours(1)
+                );
+
+            var tokenString = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return new Token(tokenString);
+        }
+    }
+}
